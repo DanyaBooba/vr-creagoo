4b20146241f1ce91eb22c09ba1d0a1770307845f
DanyaBooba <danil.dybko@gmail.com>
2021-11-29 20:24:57 +0300

    array work. json

--- server__5/data/script.js
+++ server__5/data/script.js
@@ -5,19 +5,21 @@ let loopLength = 5;
 
 let countObjects = 18;
 
-let jsonFile = 'data/data_file.txt';
+let jsonFile = 'data/json_data.txt';
 let idName = 'text';
 
 $(function() {
     var scene = document.querySelector('a-scene');
     //DrawAxis();
     SetID();
-
-    /*
+    ReData();
+    
+/*
     setInterval(function() {
         ReData();
     }, interval);
     */
+    
    
     function ReData() {
       var data = null;
@@ -31,6 +33,41 @@ $(function() {
           console.log('File is empty');
         }
         else if(data !== null){
+
+
+            var root = data["RootNode"];
+            var objects__1 = root["Lines"];
+
+            console.log(data);
+            console.log(root);
+            console.log('f');
+            //console.log(objects__1[0].Childs[0].Childs[1].Childs[0]);
+            console.log(objects__1);
+
+            var hospital__2 = objects__1[1]["Childs"][0];
+            var factory__1 = objects__1[1]["Childs"][1];
+            var factory__2 = objects__1[1]["Childs"][2];
+
+            var home__1 = objects__1[0].Childs[0].Childs[1].Childs[0];
+            var home__2 = objects__1[0].Childs[0].Childs[1].Childs[1];
+            var home__3 = objects__1[1]["Childs"][3];
+            var home__4 = objects__1[1]["Childs"][4];
+            var home__5 = objects__1[1]["Childs"][5];
+            var home__6 = objects__1[1]["Childs"][6];
+
+            console.log("Обьекты: ");
+            console.log(home__1);
+            console.log(home__2);
+            console.log(home__3);
+            console.log(home__4);
+            console.log(home__5);
+            console.log(home__6);
+            console.log(hospital__2);
+            console.log(factory__1);
+            console.log(factory__2);
+
+
+            /*
             for (var i = 1; i < loopLength; i++) {
                 if (data['x' + i] != undefined && data['y' + i] != undefined && data['z' + i] != undefined) {
                   if ($('#' + idName + i).length) {
@@ -44,12 +81,16 @@ $(function() {
                   }
                 }
               }
+              */
+
+
         }
       });
 
     }
 
     function SetID(){
+
       for (var i = 1; i < countObjects; i++) {
         if ($('#' + idName + i).length) {
 
@@ -58,6 +99,11 @@ $(function() {
           });
         }
       }
+
+    }
+
+    function ToNewArray(){
+
     }
 
     function CreateElement(id, x, y, z) {
